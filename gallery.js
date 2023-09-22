6440ffecf47ef69a5176e4c5b7b1aa2a711ee2e0
xhywebdev <143572252+xhywebdev@users.noreply.github.com>
2023-09-21 21:52:01 -0700

    added description box

--- gallery.js
+++ gallery.js
@@ -1,18 +1,46 @@
 var photoList;
 var listElementHTML;
-var photo;
+var description;
+var infobox;
+var closeBtn;
+var title;
 
 photoList = document.getElementById('photoList');
-photo = document.getElementsByClassName('photo');
 
 function addPhoto() {
     for (i = 1; i <= 12; i++) {
-        listElementHTML = `<li><img class="photo" src="images/gallery/pet${i}.jpg"><span class="caption">Pet ${i}</span><span class="description">Adorable animal</span></li>`;
+        listElementHTML = `
+        <li>
+            <img class="photo" src="images/gallery/pet${i}.jpg">
+            <span class="caption">Pet ${i}</span>
+            <span class="description">Adorable animal</span>
+        </li>`;
         photoList.innerHTML += listElementHTML
     }
 }
-
 addPhoto();
+
+
+
+description = document.getElementsByClassName('description');
+infoBox = document.getElementById('infoBox');
+closeBtn = document.getElementById('closeBtn');
+title = document.getElementById('title');
+
+for (j = 0; j < 12; j++) {
+    description[j].addEventListener("click", function () {
+        // console.log('clicked');
+        // console.log(infoBox);
+        // infoBox.innerHTML = 'Hiiiiiii';
+        infoBox.style.display = "block";
+
+    })
+}
+
+
+closeBtn.addEventListener("click", function () {
+    infoBox.style.display = "none";
+})
 
 
 // for (p of photo) {
